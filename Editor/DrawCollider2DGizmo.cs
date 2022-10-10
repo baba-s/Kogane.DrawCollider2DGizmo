43327b96d80f14b7bc375c369bd809c3d74369e6
baba-s <shotababa1206@gmail.com>
2022-10-10 11:34:58 +0900

    update

--- Editor/DrawCollider2DGizmo.cs
+++ Editor/DrawCollider2DGizmo.cs
@@ -9,17 +9,33 @@ namespace Kogane.Internal
         private static void DrawBoxCollider2D( BoxCollider2D collider, GizmoType type )
         {
             var setting = DrawCollider2DGizmoSetting.instance;
+
             if ( !setting.IsEnable ) return;
-            Gizmos.color = setting.Color;
 
             var bounds = collider.bounds;
             var min    = bounds.min;
             var max    = bounds.max;
 
-            Gizmos.DrawLine( min, new( max.x, min.y, min.z ) );
-            Gizmos.DrawLine( min, new( min.x, max.y, min.z ) );
-            Gizmos.DrawLine( max, new( min.x, max.y, max.z ) );
-            Gizmos.DrawLine( max, new( max.x, min.y, min.z ) );
+            DrawLine( min, new( max.x, min.y, min.z ) );
+            DrawLine( min, new( min.x, max.y, min.z ) );
+            DrawLine( max, new( min.x, max.y, max.z ) );
+            DrawLine( max, new( max.x, min.y, min.z ) );
+        }
+
+        private static void DrawLine( Vector3 from, Vector3 to )
+        {
+            var setting = DrawCollider2DGizmoSetting.instance;
+
+            Handles.DrawBezier
+            (
+                startPosition: from,
+                endPosition: to,
+                startTangent: from,
+                endTangent: to,
+                color: setting.Color,
+                texture: null,
+                width: setting.LineWidth
+            );
         }
     }
 }
--- Editor/DrawCollider2DGizmoSetting.cs
+++ Editor/DrawCollider2DGizmoSetting.cs
@@ -7,10 +7,12 @@ namespace Kogane.Internal
     internal sealed class DrawCollider2DGizmoSetting : ScriptableSingleton<DrawCollider2DGizmoSetting>
     {
         [SerializeField] private bool  m_isEnable;
-        [SerializeField] private Color m_color = Color.white;
+        [SerializeField] private Color m_color     = Color.white;
+        [SerializeField] private float m_lineWidth = 1;
 
-        public bool  IsEnable => m_isEnable;
-        public Color Color    => m_color;
+        public bool  IsEnable  => m_isEnable;
+        public Color Color     => m_color;
+        public float LineWidth => m_lineWidth;
 
         public void Save()
         {
